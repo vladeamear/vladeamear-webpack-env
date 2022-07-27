cc22bdcefd98e623e2c465884966c6165549f9a6
Владимир Мясин <v.miasin@kokoc.tech>
2022-07-27 17:45:56 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const root = document.getElementById('root')
+root.innerHTML = `
+  <h1>Webpack boilerplate first draft</h1>
+`
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+
+module.exports = {
+
+  // точка входа в приложение
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+
+  // точка выхода - помещаются скомпилированные файлы
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './template.html'), // шаблон
+      filename: 'index.html', // название выходного файла
+    }),
+    new CleanWebpackPlugin(),
+  ],
+
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      // изображения
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      // шрифты и SVG
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      // CSS, PostCSS, Sass
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
+
+  devtool: "source-map",
+
+}
